bb795e790db3986de1ebca6c531503e01668ef55
MollieS <mollie.c.stephenson@gmail.com>
2015-05-28 11:24:14 +0100

    practicing the event function and the affect of submit button

--- api.js
+++ api.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   console.log("Hello");
+  $('#cityname').submit(function(event){
+    console.log("Hi");
+    // works for a second.  submit button refreshing page&losing data
+
+  });
 });
